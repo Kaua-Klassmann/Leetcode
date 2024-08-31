e82ab57ebf565f38dfaa18bbaa2f444a5962c3cd
Kauã Klassmann <kaua.klassmann661@gmail.com>
2024-08-31 15:23:15 -0300

    1108

---
+++ problems/1108.rs
@@ -0,0 +1,18 @@
+pub fn defang_i_paddr(address: String) -> String {
+    address.replace(".", "[.]")
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::defang_i_paddr;
+
+    #[test]
+    fn test_1() {
+        assert_eq!(defang_i_paddr("1.1.1.1".to_string()), "1[.]1[.]1[.]1".to_string())
+    }
+
+    #[test]
+    fn test_2() {
+        assert_eq!(defang_i_paddr("255.100.50.0".to_string()), "255[.]100[.]50[.]0".to_string())
+    }
+}
