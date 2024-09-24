c001a64a606c65d443677858732ada5cfb1c55db
Kauã Klassmann <kaua.klassmann661@gmail.com>
2024-09-24 16:34:01 -0300

    347

--- problems/347.rs
+++ problems/347.rs
@@ -4,20 +4,20 @@ pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
     let mut counter: HashMap<i32, i32> = HashMap::new();
 
     nums.into_iter().for_each(|num| {
-        *counter.entry(num).or_insert(0) -= 1;
+        *counter.entry(num).or_insert(1) += 1;
     });
 
     let mut heapq: BinaryHeap<(i32, i32)> = BinaryHeap::with_capacity(k as usize + 1);
 
-    counter.into_iter().for_each(|nums| {
-        heapq.push((nums.1, nums.0));
+    counter.into_iter().for_each(|(num, count)| {
+        heapq.push((-count, num));
 
         if heapq.len() > k as usize {
             heapq.pop();
         }
     });
 
-    heapq.into_iter().map(|num| num.1).collect()
+    heapq.into_iter().map(|(_, num)| num).collect()
 }
 
 #[cfg(test)]
